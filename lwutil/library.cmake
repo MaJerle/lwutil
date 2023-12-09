2ec986e8c4bbede046070a9f2bb3822951714bd9
Tilen Majerle <tilen.majerle@gmail.com>
2023-12-09 16:49:23 +0100

    Improve the cmake documentation

--- lwutil/library.cmake
+++ lwutil/library.cmake
@@ -1,3 +1,13 @@
+# 
+# This file provides set of variables for end user
+# and also generates one (or more) libraries, that can be added to the project using target_link_libraries(...)
+#
+# Before this file is included to the root CMakeLists file (using include() function), user can set some variables:
+#
+# LWUTIL_COMPILE_OPTIONS: If defined, it provide compiler options for generated library.
+# LWUTIL_COMPILE_DEFINITIONS: If defined, it provides "-D" definitions to the library build
+#
+
 # Library core sources
 set(lwutil_core_SRCS 
     ${CMAKE_CURRENT_LIST_DIR}/src/lwutil/lwutil.c
